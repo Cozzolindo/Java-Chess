0b63f5db5b59439421667f2cbfc6de920b616456
Cozzolindo <cozzolinocarlos@gmail.com>
2021-10-05 15:49:11 -0300

    Class: Position

--- ChessSystem/Main.java
+++ ChessSystem/Main.java
@@ -1,5 +1,9 @@
+import BoardSet.Position;
+
 public class Main{
     public static void main(String[] args) {
-        System.out.println("Starting Chess Attempt!");
+
+        Position pos = new Position(3,5);
+        System.out.println(pos);
     }
 }
